168839aa5554d5427aab9e829f9229fee4ea767b
xiaomaAAA <294917359@qq.com>
2018-10-14 11:23:28 +0800

    logs

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 取消 移动端300毫秒点击事件
+import fastClick from 'fastclick'
+// 适配所有浏览器
+import  'styles/reset.css'
+// 不同机型边框像素
+import  'styles/border.css'
+// 引入阿里图标
+import  'styles/iconfont.css'
+
+Vue.config.productionTip = false
+// 取消 移动端300毫秒点击事件
+fastClick.attach(document.body)   
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
