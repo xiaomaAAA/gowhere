2c878b1d162d8429fa38b2639055fccfe4c2b4b7
xiaomaAAA <294917359@qq.com>
2018-10-16 11:45:28 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,13 +1,63 @@
 <template>
-    <div class="search" id="">
-        <input type="text" placeholder="输入城市名或拼音" class="search-input">
+    <div>
+        <div class="search" id="">
+            <input type="text" placeholder="输入城市名或拼音" class="search-input" v-model="keyword">
+        </div>
+        <div class="search-content" ref="search"  v-show="keyword">
+            <ul>
+                <li v-for="item of list"  class="search-item border-bottom" :key="item.id">{{item.name}}</li>
+                <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 
 <script>
+    import Bscroll from 'better-scroll'
+
     export default {
-       name: 'CitySearch'
+       name: 'CitySearch',
+       props:{
+           cities:Object
+       },
+       data () {
+            return{
+                keyword: '',
+                list: [],
+                timer:null
+            }
+       },
+       computed:{
+           hasNoData(){
+               return !this.list.length
+           }
+       },
+       watch: {
+            keyword () {
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+            if (!this.keyword) {
+                this.list = []
+                return
+            }
+            this.timer = setTimeout(() => {
+                const result = []
+                for (let i in this.cities) {
+                this.cities[i].forEach((value) => {
+                    if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                    result.push(value)
+                    }
+                })
+                }
+                this.list = result
+            }, 100)
+            }
+        },
+        mounted () {
+            this.scroll = new Bscroll(this.$refs.search)
+        }
     }
 </script>
 
